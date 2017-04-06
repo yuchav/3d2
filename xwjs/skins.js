7f173f9857d00374c0bf753a43793719728038c6
yuxiao <yuxiao@zhongan.com>
2017-04-06 13:27:50 +0800

    good

--- xwjs/skins.js
+++ xwjs/skins.js
@@ -1,114 +1,195 @@
 var sks = [{
+    chapter: '0',
+    details: {
+        h: '好孩子门店',
+        p: 'Shanghai'
+    },
+    loadingMedia: 'img/panorama/luyi1/mobile_f.jpg',
+    media: [
+        'img/panorama/luyi1/mobile_f.jpg',
+        'img/panorama/luyi1/mobile_r.jpg',
+        'img/panorama/luyi1/mobile_b.jpg',
+        'img/panorama/luyi1/mobile_l.jpg',
+        'img/panorama/luyi1/mobile_u.jpg',
+        'img/panorama/luyi1/mobile_d.jpg'
+    ],
+    hotSpots: {
+        images: [{
+                face: 'n',
+                x: '72%',
+                y: '50%',
+                media: 'img/panorama/luyi1/mobile_f.jpg',
+                caption: '示例地点'
+            }]
+            /*,
+                    videos: [{
+                        face: 'n',
+                        x: '90%',
+                        y: '90%',
+                        media: {
+                            mp4: 'http://appmedia.qq.com/media/641010718/panorama/video/umbrella-video.mp4'
+                        },
+                        pic: 'img/panorama/yuxiao_u.jpeg',
+                        caption: '视频'
+                    }]*/
+    },
+    range: {
+        y: 180
+    }
+}, {
     chapter: '1',
     details: {
-        h: '陆家嘴',
-        p:'Shanghai'
+        h: '好孩子秀台',
+        p: 'Shanghai'
     },
-    loadingMedia: 'img/panorama/lujiazui/mobile_f.jpg',
+    loadingMedia: 'img/panorama/luyi2/mobile_f.jpg',
     media: [
-        'img/panorama/lujiazui/mobile_f.jpg',
-        'img/panorama/lujiazui/mobile_r.jpg',
-        'img/panorama/lujiazui/mobile_b.jpg',
-        'img/panorama/lujiazui/mobile_l.jpg',
-        'img/panorama/lujiazui/mobile_u.jpg',
-        'img/panorama/lujiazui/mobile_d.jpg'
+        'img/panorama/luyi2/mobile_f.jpg',
+        'img/panorama/luyi2/mobile_r.jpg',
+        'img/panorama/luyi2/mobile_b.jpg',
+        'img/panorama/luyi2/mobile_l.jpg',
+        'img/panorama/luyi2/mobile_u.jpg',
+        'img/panorama/luyi2/mobile_d.jpg'
     ],
     hotSpots: {
         images: [{
-            face: 'n',
-            x: '72%',
-            y: '50%',
-            media: 'img/panorama/lujiazui/mobile_f.jpg',
-            caption: '上海中心'
-        }]/*,
-        videos: [{
-            face: 'n',
-            x: '90%',
-            y: '90%',
-            media: {
-                mp4: 'http://appmedia.qq.com/media/641010718/panorama/video/umbrella-video.mp4'
-            },
-            pic: 'img/panorama/yuxiao_u.jpeg',
-            caption: '视频'
-        }]*/
+                face: 'n',
+                x: '72%',
+                y: '50%',
+                media: 'img/panorama/luyi1/mobile_f.jpg',
+                caption: '示例地点'
+            }]
+            /*,
+                    videos: [{
+                        face: 'n',
+                        x: '90%',
+                        y: '90%',
+                        media: {
+                            mp4: 'http://appmedia.qq.com/media/641010718/panorama/video/umbrella-video.mp4'
+                        },
+                        pic: 'img/panorama/yuxiao_u.jpeg',
+                        caption: '视频'
+                    }]*/
     },
     range: {
         y: 180
     }
-}, {
-    chapter: '2',
-    details: {
-        h: '欢乐谷',
-        p: 'Sheshan'
-    },
-    loadingMedia: 'img/panorama/huanlegu/mobile_f.jpg',
-    media: [
-        'img/panorama/huanlegu/mobile_f.jpg',
-        'img/panorama/huanlegu/mobile_r.jpg',
-        'img/panorama/huanlegu/mobile_b.jpg',
-        'img/panorama/huanlegu/mobile_l.jpg',
-        'img/panorama/huanlegu/mobile_u.jpg',
-        'img/panorama/huanlegu/mobile_d.jpg'
-    ],
-    hotSpots: {
-        /*images: [{
-            face: 'n',
-            x: '50%',
-            y: '50%',
-            media: 'img/panorama/yuxiao_d.jpeg',
-            caption: '图'
-        }],
-        videos: [{
-            face: 'n',
-            x: '75%',
-            y: '75%',
-            media: {
-                mp4: 'http://appmedia.qq.com/media/641010718/Windows15sec.mp4'
-            },
-            pic: 'img/panorama/yuxiao_d.jpeg',
-            caption: '视频'
-        }]*/
-    },
-    range: {
-        y: 180
-    }
-}, {
-    chapter: '3',
-    details: {
-        h: '民宿',
-        p: 'YunNan'
-    },
-    loadingMedia: 'img/panorama/minsu/mobile_f.jpg',
-    media: [
-        'img/panorama/minsu/mobile_f.jpg',
-        'img/panorama/minsu/mobile_r.jpg',
-        'img/panorama/minsu/mobile_b.jpg',
-        'img/panorama/minsu/mobile_l.jpg',
-        'img/panorama/minsu/mobile_u.jpg',
-        'img/panorama/minsu/mobile_d.jpg'
-    ],
-    hotSpots: {
-        /*
-        images: [{
-            face: 'n',
-            x: '50%',
-            y: '50%',
-            media: 'img/panorama/yuxiao_u.jpeg',
-            caption: '图'
-        }],
-        videos: [{
-            face: 'n',
-            x: '90%',
-            y: '90%',
-            media: {
-                mp4: 'http://appmedia.qq.com/media/641010718/panorama/video/umbrella-video.mp4'
-            },
-            pic: 'img/panorama/yuxiao_u.jpeg',
-            caption: '视频'
-        }]
-        */
-    },
-    range: {
-        y: 180
-    }
-}]
+}
+
+// , {
+//     chapter: '2',
+//     details: {
+//         h: '陆家嘴',
+//         p: 'Shanghai'
+//     },
+//     loadingMedia: 'img/panorama/lujiazui/mobile_f.jpg',
+//     media: [
+//         'img/panorama/lujiazui/mobile_f.jpg',
+//         'img/panorama/lujiazui/mobile_r.jpg',
+//         'img/panorama/lujiazui/mobile_b.jpg',
+//         'img/panorama/lujiazui/mobile_l.jpg',
+//         'img/panorama/lujiazui/mobile_u.jpg',
+//         'img/panorama/lujiazui/mobile_d.jpg'
+//     ],
+//     hotSpots: {
+//         images: [{
+//                 face: 'n',
+//                 x: '72%',
+//                 y: '50%',
+//                 media: 'img/panorama/lujiazui/mobile_f.jpg',
+//                 caption: '上海中心'
+//             }]
+//             /*,
+//                     videos: [{
+//                         face: 'n',
+//                         x: '90%',
+//                         y: '90%',
+//                         media: {
+//                             mp4: 'http://appmedia.qq.com/media/641010718/panorama/video/umbrella-video.mp4'
+//                         },
+//                         pic: 'img/panorama/yuxiao_u.jpeg',
+//                         caption: '视频'
+//                     }]*/
+//     },
+//     range: {
+//         y: 180
+//     }
+// }, {
+//     chapter: '3',
+//     details: {
+//         h: '欢乐谷',
+//         p: 'Sheshan'
+//     },
+//     loadingMedia: 'img/panorama/huanlegu/mobile_f.jpg',
+//     media: [
+//         'img/panorama/huanlegu/mobile_f.jpg',
+//         'img/panorama/huanlegu/mobile_r.jpg',
+//         'img/panorama/huanlegu/mobile_b.jpg',
+//         'img/panorama/huanlegu/mobile_l.jpg',
+//         'img/panorama/huanlegu/mobile_u.jpg',
+//         'img/panorama/huanlegu/mobile_d.jpg'
+//     ],
+//     hotSpots: {
+//         /*images: [{
+//             face: 'n',
+//             x: '50%',
+//             y: '50%',
+//             media: 'img/panorama/yuxiao_d.jpeg',
+//             caption: '图'
+//         }],
+//         videos: [{
+//             face: 'n',
+//             x: '75%',
+//             y: '75%',
+//             media: {
+//                 mp4: 'http://appmedia.qq.com/media/641010718/Windows15sec.mp4'
+//             },
+//             pic: 'img/panorama/yuxiao_d.jpeg',
+//             caption: '视频'
+//         }]*/
+//     },
+//     range: {
+//         y: 180
+//     }
+// }, {
+//     chapter: '4',
+//     details: {
+//         h: '民宿',
+//         p: 'YunNan'
+//     },
+//     loadingMedia: 'img/panorama/minsu/mobile_f.jpg',
+//     media: [
+//         'img/panorama/minsu/mobile_f.jpg',
+//         'img/panorama/minsu/mobile_r.jpg',
+//         'img/panorama/minsu/mobile_b.jpg',
+//         'img/panorama/minsu/mobile_l.jpg',
+//         'img/panorama/minsu/mobile_u.jpg',
+//         'img/panorama/minsu/mobile_d.jpg'
+//     ],
+//     hotSpots: {
+//         /*
+//         images: [{
+//             face: 'n',
+//             x: '50%',
+//             y: '50%',
+//             media: 'img/panorama/yuxiao_u.jpeg',
+//             caption: '图'
+//         }],
+//         videos: [{
+//             face: 'n',
+//             x: '90%',
+//             y: '90%',
+//             media: {
+//                 mp4: 'http://appmedia.qq.com/media/641010718/panorama/video/umbrella-video.mp4'
+//             },
+//             pic: 'img/panorama/yuxiao_u.jpeg',
+//             caption: '视频'
+//         }]
+//         */
+//     },
+//     range: {
+//         y: 180
+//     }
+// }
+
+]
